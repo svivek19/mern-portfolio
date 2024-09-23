3b28c99de3686f57f4423d39680955eb1d3a1ddb
svivek19 <svivek030503@gmail.com>
2024-09-23 16:12:44 +0530

    get api and update api

--- server/controller/data.controller.js
+++ server/controller/data.controller.js
@@ -15,4 +15,34 @@ const createData = async (req, res) => {
   }
 };
 
-module.exports = { createData };
+// Get all data
+const getAllData = async (req, res) => {
+  try {
+    const data = await Data.find();
+    return res.status(200).json({ data });
+  } catch (error) {
+    return res
+      .statustatus(500)
+      .json({ message: "Error fetching data", error: error.message });
+  }
+};
+
+// Update data by _id
+const updateData = async (req, res) => {
+  const { _id } = req.params;
+  const updateData = req.body;
+  try {
+    const response = await Data.findByIdAndUpdate(
+      _id,
+      { $set: updateData },
+      { new: true }
+    );
+    return response.status(200).json({ message: "Data updated successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error updating data", error: error.message });
+  }
+};
+
+module.exports = { createData, getAllData, updateData };
--- server/routes/data.route.js
+++ server/routes/data.route.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { createData } = require("../controller/data.controller");
+const {
+  createData,
+  getAllData,
+  updateData,
+} = require("../controller/data.controller");
+
+// get data
+router.get("/", getAllData);
 
 // post data
 router.post("/addEdit", createData);
+
+// update data
+router.put("/update/:_id", updateData);
 
 module.exports = router;
